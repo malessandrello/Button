1e90f06d807dddb35f9cb2d807d59e759a6e5ca4
malessandrello <mauricio.alessandrello@gmail.com>
2024-01-28 13:49:25 -0300

    minor changes

--- Rscripts/análisis.R
+++ Rscripts/análisis.R
@@ -8,13 +8,16 @@ Button <- Button %>% setNames(c("num", "tiempo", "temp" ))
 max <- Button$temp[which.max(Button$temp)]
 min <- Button$temp[which.min(Button$temp)]
 promedio <- mean(Button$temp)
-quantile(Button$temp, c(seq(0, 1, 0.01)))
+quantile(Button$temp, c(seq(0, 1, 0.05)))
 max
 min
 promedio
 menores <- Button[which(Button$temp < quantile(Button$temp, 0.05)), ]
 mayores <- Button[which(Button$temp > quantile(Button$temp, 0.95)), ]
 Button %>% ggplot(aes(tiempo, temp)) + geom_point()
+Button %>% filter(temp < 34.0)
+
+
 
 
 
